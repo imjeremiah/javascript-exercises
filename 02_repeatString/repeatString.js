3ba2b34869cf82662c65171bd1ec5006ce429c45
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-19 15:22:50 -0700

    Complete JavaScript Exercise 02- Repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,28 @@
-const repeatString = function() {
 
+// using .repeat() method:
+
+// const repeatString = function(string, num) {
+//     if (num < 0) {
+//         return "ERROR";
+//     } else {
+//         return string.repeat(num);
+//     }
+// };
+
+// using a loop:
+
+const repeatString = function(string, num) {
+
+    let phrase = "";
+
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < num; i ++) {
+            phrase = phrase + string;
+        }
+        return phrase;
+    }
 };
 
 // Do not edit below this line
