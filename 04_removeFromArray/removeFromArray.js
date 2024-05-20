a84d09abcbb0d7c72c0a0ccb1baeb2df46dc6336
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-19 17:48:52 -0700

    Complete JavaScript Exercise 04- Remove from Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+    for (const item of array) {
+        for (const arg of args) {
+            if (array.includes(arg)) {
+                array.splice(array.indexOf(arg), 1);
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
