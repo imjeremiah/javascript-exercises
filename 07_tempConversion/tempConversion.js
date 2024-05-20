015204a645a008703ab1f598569a22dbc1ffbb9d
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-19 18:24:00 -0700

    Complete JavaScript Exercise 07- Temp Conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let degrees = (temp - 32) * (5/9);
+
+  return (Math.round(degrees * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let degrees = (temp * 9/5) + 32;
+  
+  return (Math.round(degrees * 10) / 10);
 };
 
 // Do not edit below this line
