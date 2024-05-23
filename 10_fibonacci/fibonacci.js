d8dd440b088cd373e1ca25ae7a9e69952373a5a1
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-23 09:17:15 -0700

    Complete JavaScript Exercise 10- Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    if (num === 1) return 1;
 
+    let curr = 1;
+    let num1 = 1;
+    let num2 = 1;
+
+    for (let i = 2; i < num; i++) {
+        num2 = num1;
+        num1 = curr;
+        curr = num1 + num2;
+    }
+    return curr;
 };
 
 // Do not edit below this line
