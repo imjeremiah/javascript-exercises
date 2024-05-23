6b51e1a4de47edf5130708b38adacfe24e4bfe1c
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-22 17:23:12 -0700

    Complete JavaScript Exercise 08- Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) return 0;
+	return array.reduce((acc, curr) => acc + curr);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) return 1;
+
+	let factorial = num;
+
+  while (num > 1) {
+    factorial *= (num - 1);
+    num--;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
