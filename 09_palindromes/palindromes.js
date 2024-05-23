37a925fdcf90f535fdbf3edd08b9c1dd883f9542
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-22 17:40:15 -0700

    Complete JavaScript Exercise 09- Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let updatedString = string.replace(/[\.,?!-\s]/g, "").toLowerCase();
+    let reversedString = updatedString.split("").reverse().join("");
+    console.log(updatedString);
 
+    return reversedString === updatedString;
 };
 
 // Do not edit below this line
