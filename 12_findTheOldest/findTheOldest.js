c8f4d75228aeebe39616c1c392635c87a65e32e9
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-23 09:44:34 -0700

    Complete JavaScript Exercise 12- Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for (const person of people) {
+        person.age = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+    }
+    
+    return people.sort((a, b) => b.age - a.age)[0];
 };
 
 // Do not edit below this line
